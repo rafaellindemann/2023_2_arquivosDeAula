9faa17c4b19156bfe86bf06a518224e8da4c90b2
rafaellindemann <rafaelld@gmail.com>
2023-09-06 17:56:53 -0300

    Tela do conversor numérico com objetos CSS por componente; Construído com a turma V1

---
+++ 2a/V1/convNumV1/objeto.js
@@ -0,0 +1,10 @@
+const animalzinho = {
+    nome: 'Gato',
+    idade: 2,
+    peso: 4.5,
+    cor: 'Preto'
+}
+
+Possíveis relatórios: 
+ - Clientes devedores;
+ - Animais que não se vacinaram nos últimos 12 meses;
