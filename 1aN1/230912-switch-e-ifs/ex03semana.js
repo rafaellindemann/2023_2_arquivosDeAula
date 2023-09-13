94c934b212fac90c2d576e2031432f96b42edc63
Professor <Professor@SN-371154.edu.fiesc.com.br>
2023-09-12 22:11:27 -0300

    Exercícios seleções resolvidos

---
+++ 1aN1/230912-switch-e-ifs/ex03semana.js
@@ -0,0 +1,20 @@
+
+let dia 
+dia = Number(prompt("Digite o número do dia: "))
+switch (dia) {
+    case 1: alert("Domingo!")
+        break
+    case 2: alert("Segunda")
+        break
+    case 3: alert("Terça")
+        break
+    case 4: alert("Quarta")
+        break
+    case 5: alert("Quinta") 
+        break
+    case 6: alert("Sexta")
+        break
+    case 7: alert("Sábado")
+        break
+    default: alert("Dia inválido!")
+}
