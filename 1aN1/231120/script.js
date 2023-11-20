47e5540b9c6c7c2fc1f157d99966f083bcaa2036
rafaellindemann <rafaelld@gmail.com>
2023-11-20 19:54:56 -0300

    Vetores intro

---
+++ 1aN1/231120/script.js
@@ -0,0 +1,14 @@
+let pets = []
+
+let inputPet = document.getElementById('inpPet')
+
+function cadastrar(){
+    // ler o valor no input
+    let pet = inputPet.value
+
+    // empurrar pra dentro do vetor
+    pets.push(pet)
+
+    
+
+}
