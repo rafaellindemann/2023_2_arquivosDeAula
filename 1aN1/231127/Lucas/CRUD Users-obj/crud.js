bd129e51153d0f738807d76b440422d4601ce6bc
rafaellindemann <rafaelld@gmail.com>
2023-11-27 21:57:44 -0300

    O melhor crud de todos os tempos

---
+++ 1aN1/231127/Lucas/CRUD Users-obj/crud.js
@@ -0,0 +1,95 @@
+// Vetor onde ficarão os usuários 
+
+
+let usuarios = []
+
+// Pega os elementos pelos seus ID's e armazena em variáveis
+let elementoInputUser = document.getElementById('inptUser')
+let elementoInputCPF = document.getElementById('inptCPF')
+let elementoInputIdade = document.getElementById('inptIdade')
+
+let elementoDivLista = document.getElementById('lista')
+let elementoInputPesquisa = document.getElementById('inptPesquisa')
+
+// Variáveis 
+let userPesquisa = ''
+let posPesquisa = ''
+
+// Cadastrar
+function Cadastrar(){
+
+    let usuario = {
+        nome: elementoInputUser.value,
+        cpf: elementoInputCPF.value,
+        idade: elementoInputIdade.value
+    }
+
+    usuarios.push(usuario)
+
+    // users.push(elementoInputUser.value)
+    // cpfs.push(elementoInputCPF.value)
+    // idades.push(elementoInputIdade.value)
+    Listar()
+    alert('Deu bom! =D')
+    elementoInputUser.value = ''
+    elementoInputCPF.value = ''
+    elementoInputIdade.value = ''
+    elementoInputUser.focus()
+}
+
+// Listar
+function Listar(){
+
+    elementoDivLista.innerHTML = ''
+    for(i=0; i<usuarios.length; i++){
+        elementoDivLista.innerHTML +=  
+        `<div class="card">
+            Nome: ${usuarios[i].nome} <br>
+            CPF: ${usuarios[i].cpf} <br>
+            Idade: ${usuarios[i].idade} 
+        </div>`
+    }
+
+    // elementoDivLista.innerHTML = users + '<br>'
+    // elementoDivLista.innerHTML += cpfs + '<br>'
+    // elementoDivLista.innerHTML += idades
+}
+
+// Pesquisar
+function Pesquisar(){
+
+    userPesquisa = elementoInputPesquisa.value
+    posPesquisa = users.indexOf(userPesquisa)
+    
+    if(posPesquisa != -1){
+        elementoInputUser.value = userPesquisa
+        elementoInputPesquisa.value = ''
+        alert('Usuário encontrado! :)')
+    }else{
+        alert('Usuário NÃO encontrado! :)')
+    }
+
+}
+
+// Editar
+function Editar(){
+
+    users[posPesquisa] = elementoInputUser.value
+    Listar()
+    alert('Usuário editado! :B')
+
+}
+
+// Deletar
+function Deletar(){
+
+    users.splice(posPesquisa, 1)
+    Listar()
+    alert('Usuário deletado! :X')
+
+}
+
+
+
+
+
